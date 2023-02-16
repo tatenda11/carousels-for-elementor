a866bd1b071c1e4332080b5f46a2a096b7dda498
Tatenda Munenge <tatenda.munenge@stickee.co.uk>
2023-02-17 00:04:41 +0200

    Update slider logic

--- assets/js/tabbed-carousel.js
+++ assets/js/tabbed-carousel.js
@@ -1,26 +1,29 @@
 jQuery( document ).ready(function( $ ) {
-    $('.your-class').slick({
+    $('.tabbed-carousel-slide').slick({
         centerMode:false,     
         draggable:false,    
         arrows:true,         
         slidesToShow:1,     
         slidesToScroll:1,   
-        dots:true,
+        dots:false,
     });
 
-    $('.carousel-wrapper').each(function(index){
-        if(index > 0) {
-            $(this).fadeOut();
-        }
-    }).promise().done( function(){ $('.tabbed-carousel-section').removeClass('hide')} );
+    $('.carousel-tab-nav-button').click(function(e){
+        e.preventDefault();
+        var index = $(this).data('slide-index');
+        $('.tabbed-carousel-slide').slick('slickGoTo', index);
+    });
+
+    $('.tabbed-carousel-section').removeClass('hide');
 
     $('.carousel-tab-nav-button').click(function(e){
         e.preventDefault();
         $('.carousel-tab-nav-button').removeClass('active-slide-button');
         $(this).addClass('active-slide-button');
-        var slideIndex = $(this).data('slide-index');
-        $(".carousel-wrapper").hide();
-        //$('.carousel-wrapper').addClass('hide');
-        $('#section-'+ slideIndex).fadeIn(500);
+    });
+
+    $('.tabbed-carousel-slide').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $('.carousel-tab-nav-button').removeClass('active-slide-button');
+        $('#tab-button-' + nextSlide).addClass('active-slide-button');
     });
 });
--- widgets/tabbed-carousel-widget.php
+++ widgets/tabbed-carousel-widget.php
@@ -93,6 +93,19 @@ class Elementor_Tabbed_Carousel_Widget extends \Elementor\Widget_Base {
 	 */
 	private function getCarouselFields() {
 		$repeater = new \Elementor\Repeater();
+
+		$repeater->add_control(
+			'slide_label',
+			[
+				'label' => esc_html__( 'Slide Button', 'elementor-tabbed-carousel-widget' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'placeholder' => esc_html__( 'Slide Button', 'elementor-tabbed-carousel-widget' ),
+				'dynamic' => [
+					'active' => true,
+				],
+			]
+		); 
+
 		$repeater->add_control(
 			'slide_image',
 			[
@@ -153,58 +166,30 @@ class Elementor_Tabbed_Carousel_Widget extends \Elementor\Widget_Base {
 	 * @access protected
 	 */
 	protected function register_controls() {
-     	$this->start_controls_section(
+		$this->start_controls_section(
 			'content_section',
 			[
-				'label' => esc_html__( 'Carousel Content', 'elementor-tabbed-carousel-widget' ),
+				'label' => esc_html__( 'Carousel Content', 'elementor-list-widget' ),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
 
-		$repeater = new \Elementor\Repeater();
-
-		
-		$repeater->add_control(
-			'carousel_group_label',
+		$this->add_control(
+			'content_slides',
 			[
-				'label' => esc_html__( 'Button Label', 'elementor-tabbed-carousel-widget' ),
-				'type' => \Elementor\Controls_Manager::TEXT,
-				'input_type' => 'text',
-				'placeholder' => esc_html__( 'My button', 'elementor-tabbed-carousel-widget' ),
-			]
-		);
-
-		$repeater->add_control(
-			'carousel_slides',
-			[
-				'label' => esc_html__( 'Carousel Slides', 'elementor-tabbed-carousel-widget' ),
+				'label' => esc_html__( 'Carousel Items', 'elementor-list-widget' ),
 				'type' => \Elementor\Controls_Manager::REPEATER,
-				'fields' => $this->getCarouselFields()->get_controls(),           
-				'title_field' => '{{{ slide_heading }}}',
+				'fields' => $this->getCarouselFields()->get_controls(),/* Use our repeater */
 				'default' => [
 					[
-						'slide_image' => 'https://picsum.photos/200/300.jpg',
-						'slide_heading' => 'Slide Heading 1',
-						'slide_description' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum',
-						'slide_call_to_action' => '#',
-					],
-					[
-						'slide_image' => 'https://picsum.photos/200/300.jpg',
-						'slide_heading' => 'Slide Heading 2',
-						'slide_description' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum',
-						'slide_call_to_action' => '#',
+						'slide_image' => 'https://placehold.co/600x400',
+						'slide_label' => 'Button',
+						'slide_heading' => 'Heading One',
+						'slide_description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
+						'slide_call_to_action' => '#'
 					],
 				],
-			]
-		);
-		
-
-		$this->add_control(
-			'carousel_sections',
-			[
-				'label' => esc_html__( 'List Items', 'elementor-tabbed-carousel-widget' ),
-				'type' => \Elementor\Controls_Manager::REPEATER,
-				'fields' => $repeater->get_controls(),           
+				'title_field' => '{{{ slide_heading }}}',
 			]
 		);
 
@@ -213,46 +198,40 @@ class Elementor_Tabbed_Carousel_Widget extends \Elementor\Widget_Base {
 
 	protected function render() {
 		$settings = $this->get_settings_for_display();
-		$fields = $settings['carousel_sections'];
+		$fields = $settings['content_slides'];
 		?>
 			<div class='tabbed-carousel-section hide'>
 				<div class='carousel-tab-nav'>
 					<ul  class='carousel-tab-nav-list'>
 						<?php foreach ( $fields as $index => $field ) { ?>
 							<li>
-								<button class='carousel-tab-nav-button <?php if($index === 0 ) { echo "active-slide-button";}?>' data-slide-index="<?php echo $index;?>"><?php echo $field['carousel_group_label']; ?></button>
+								<button id="tab-button-<?php echo $index;?>" class='carousel-tab-nav-button <?php if($index === 0 ) { echo "active-slide-button";}?>' data-slide-index="<?php echo $index;?>"><?php echo $field['slide_label']; ?></button>
 							</li>
 						<?php } ?>
 					</ul>
 				</div>
-				<div class='carousel-wrapper-container'>
-					<?php foreach ( $fields as $index => $field ) { ?>
-						<div id="section-<?php echo $index;?>" class='carousel-wrapper carousel-section <?php if($index === 0 ) { echo "active-slide-section";}?>' data-slide-index="<?php echo $index;?>">
-							<div class="your-class" style="width:100%;min-height:350px;">
-								<?php foreach ( $fields[$index]['carousel_slides'] as $item ) {?>
-									<div class='carousel-slide'>
-										<div class='col-6 image-seg'>
-											<img src="<?php echo $item['slide_image']['url']; ?>" alt="<?php echo $item['slide_heading'];?>">
-										</div>
-										<div class='col-6 content-seg'>
-											<h2><?php echo $item['slide_heading'];?></h2>
-											<p>
-												<?php echo $item['slide_description'];?>
-											</p>
-											<a href="<?php echo $item['slide_call_to_action'];?>" style="background-color:#009EDB; padding:15px; color:#fff;text-decoration:none;font-weight:bold;">Read more</a>
-										</div>
-									</div>
-								<?php }?>
-							</div>	
-						</div> 
-					<?php }?> 
+				<div class="tabbed-carousel-slide" style="width:100%;min-height:350px;">
+					<?php foreach ( $fields as $index => $item ) {?>
+						<div class='carousel-slide' data-slide-index="<?php echo $index;?>">>
+							<div class='col-6 image-seg'>
+								<img src="<?php echo $item['slide_image']['url']; ?>" alt="<?php echo $item['slide_heading'];?>">
+							</div>
+							<div class='col-6 content-seg'>
+								<h2><?php echo $item['slide_heading'];?></h2>
+								<p>
+									<?php echo $item['slide_description'];?>
+								</p>
+								<a href="<?php echo $item['slide_call_to_action']['url'];?>" style="background-color:#009EDB; padding:15px; color:#fff;text-decoration:none;font-weight:bold;">Read more</a>
+							</div>
+						</div>
+					<?php }?>
 				</div>
 			</div>
 		<?php
 	}
 
 	protected function content_template() {
-		echo "I am working";
+		echo "Works\n";
 	}
 
 }
